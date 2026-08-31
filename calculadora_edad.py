5bd0be4bc363982bde0907137601c3741b1295ae
horwincb-arch <horwincb@uamv.edu.ni>
2026-08-31 10:35:33 -0600

    Se actualizo

--- calculadora_edad.py
+++ calculadora_edad.py
@@ -1,5 +1,10 @@
-try:
-    edad = int(input("Edad: "))
-    print("Edad registrada:", edad)
-except ValueError:
-    print("Ingrese un valor númerico: ")
+from colorama import Fore, Style
+while True:
+    try:
+        edad = int(input("Edad: "))
+        print("Edad registrada: ", edad)
+        break
+    except ValueError:
+        print(Fore.RED + "Ingrese un valor númerico: ")
+        print(Style.RESET_ALL)
+print(Fore.GREEN + "Edad registrda: ", edad)
